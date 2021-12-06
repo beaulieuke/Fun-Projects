54a38c35b54e0f5835eebbd3cb0f1beb470050db
Kevin Beaulieu <83260131+beaulieuke@users.noreply.github.com>
2021-12-06 10:20:50 -0500

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,20 @@
+#madlibs generator
+#Loop back to this point once code finishes
+loop = 1
+while (loop < 10):
+    noun = input("Choose a noun: ")
+    p_noun = input("choose a plural noun: ")
+    noun2 = input("Choose another noun: ")
+    place = input("Choose a place: ")
+    adjective = input("Choose a adjective: ")
+    noun3 = input("Choose a noun: ")
+#Story
+    print ("----------------------------------")
+    print ("There once was a little man",noun,"who was")
+    print ("eating",noun2,"very fast")
+    print ("he ate",noun2,"so fast that he barfed")
+    print ("he ended up puking so hard he fell to",place,)
+    print ("and he wound up dead")
+#loop back to "loop = 1"
+    loop = loop + 1
+    
